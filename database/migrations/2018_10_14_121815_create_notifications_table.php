8b577f07ba9184d111df215838090186fec2d8c0
Dipesh Gyawali <dipesh9393@gmail.com>
2018-10-27 00:55:23 +0545

    THis is the notification migration

---
+++ database/migrations/2018_10_14_121815_create_notifications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('master_id'); //logged in user
+            $table->integer('slave_id'); //who follows other
+            $table->string('notification_post');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
