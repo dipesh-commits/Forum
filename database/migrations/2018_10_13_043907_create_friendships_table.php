b0b9e171206a67dc8e89bcbecc12419189320d15
Dipesh Gyawali <dipesh9393@gmail.com>
2018-11-07 21:04:50 +0545

    Friendship database

---
+++ database/migrations/2018_10_13_043907_create_friendships_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFriendshipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friendships', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('requested_user');  //those who are being followed
+            $table->integer('requesting_user'); //those who follow other
+            $table->boolean('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friendships');
+    }
+}
